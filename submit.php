c35a62a2835ab1cc9f3f06df99de798c2d79967d
= <aleclair.1105@outlook.com>
2024-10-17 17:43:57 -0500

    Need branch seperation

---
+++ submit.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "";
+$username = '';
+$password = '';
+$dbname = '';
+
+try {
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("");
+    #try to make inputs safe from SQL injection
+    
+    $stmt->bindParam(':name',$name);
+    $stmt->bindParam(':TNumber',$TNumber);
+    
+    $name = $_POST['name'];
+    $TNumber = $_POST['TNumber'];
+    $stmt->execute();
+
+    echo 'Success';
+
+}
+
+catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+}
+
+#close the connection
+$conn = null;
+?>
